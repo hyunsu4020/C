5ff8b6f7efbaf1483a9c55ad9f00e2bd553c9c50
LeeHyunSu <76948939+hyunsu4020@users.noreply.github.com>
2022-04-28 20:10:17 +0900

    Add files via upload

---
+++ C/echo_client_err.c
@@ -0,0 +1,66 @@
+/*
+ * echo_client.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>				// errno.h 파일 include
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define BUFSIZE 1024
+void error_handling(char *message);
+
+int main(int argc, char **argv)
+{
+	int sock;
+	char message[BUFSIZE];
+	int str_len;
+	struct sockaddr_in serv_addr;
+
+	if(argc!=3){
+		printf("Usage : %s <IP> <port>\n", argv[0]);
+		exit(1);
+	}
+	
+	sock=socket(PF_INET, SOCK_STREAM, 0);   
+	if(sock == -1)
+		error_handling("socket() error");
+	
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family=AF_INET;
+	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
+	serv_addr.sin_port=htons(atoi(argv[2]));
+	
+	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
+		error_handling("connect() error!");
+	
+	while(1) {
+		/* 메세지 입력, 전송 */
+		fputs("전송할 메시지를 입력 하세요 (q to quit) : ", stdout);
+		fgets(message, BUFSIZE, stdin);
+		
+		if(!strcmp(message,"q\n"))	break;
+		write(sock, message, strlen(message));   // s="hello"; sizeof(s): 6, strlen(s): 5
+		
+		/* 메세지 수신, 출력 */
+		str_len=read(sock, message, BUFSIZE-1);
+		message[str_len]='\0';
+		printf("서버로부터 전송된 메시지 : %s \n", message);
+	}
+	
+	close(sock);
+	return 0;
+}
+
+void error_handling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	printf("Error Code(%d): %s\n", errno, strerror(errno));   // 전역변수 errno는 Error Code를 가짐
+	exit(1);
+}
+
