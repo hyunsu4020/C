091558847ac77f95bf039e468b738fe97c6f5ec3
LeeHyunSu <76948939+hyunsu4020@users.noreply.github.com>
2022-05-11 23:20:09 +0900

    Add files via upload

--- C/echo_server.c
+++ C/echo_server.c
@@ -1,67 +1,70 @@
 /*
- * echo_server.c
+ * echo_server_win.c
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+#include <winsock2.h>
 
 #define BUFSIZE 1024
-void error_handling(char *message);
+void ErrorHandling(char* message);
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-	int serv_sock;
-	int clnt_sock;
-	char message[BUFSIZE];
-	int str_len;
-	
-	struct sockaddr_in serv_addr;
-	struct sockaddr_in clnt_addr;
-	int clnt_addr_size;
-	
-	if(argc!=2){
-		printf("Usage : %s <port>\n", argv[0]);
-		exit(1);
-	}
-	
-	serv_sock=socket(PF_INET, SOCK_STREAM, 0);   
-	if(serv_sock == -1)
-		error_handling("socket() error");
-	
-	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family=AF_INET;
-	serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
-	serv_addr.sin_port=htons(atoi(argv[1]));
-	
-	if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1)
-		error_handling("bind() error");
-	
-	if(listen(serv_sock, 5)==-1)
-		error_handling("listen() error");
-	
-	clnt_addr_size=sizeof(clnt_addr);    
-	clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr,&clnt_addr_size);
-	if(clnt_sock==-1)
-		error_handling("accept() error");
-	
-	/* 데이터 수신 및 전송 */
-	while( (str_len=read(clnt_sock,message, BUFSIZE)) != 0){
-		write(clnt_sock, message, str_len);
-		write(1, message, str_len);
-	}
-	close(clnt_sock);
-	return 0;
+    WSADATA wsaData;
+    SOCKET hServSock;
+    SOCKET hClntSock;
+    char message[BUFSIZE];
+    int strLen;
+
+    SOCKADDR_IN servAddr;
+    SOCKADDR_IN clntAddr;
+    int clntAddrSize;
+
+    if (argc != 2) {
+        printf("Usage : %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) /* Load Winsock 2.2 DLL */
+        ErrorHandling("WSAStartup() error!");
+    printf("Server Running \n")
+
+    hServSock = socket(PF_INET, SOCK_STREAM, 0);
+    if (hServSock == INVALID_SOCKET)
+        ErrorHandling("socket() error");
+
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddr.sin_port = htons(atoi(argv[1]));
+
+    if (bind(hServSock, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+        ErrorHandling("bind() error");
+
+    if (listen(hServSock, 5) == SOCKET_ERROR)
+        ErrorHandling("listen() error");
+
+    clntAddrSize = sizeof(clntAddr); // Error! 추가!
+    hClntSock = accept(hServSock, (SOCKADDR*)&clntAddr, &clntAddrSize);
+    if (hClntSock == INVALID_SOCKET)
+        ErrorHandling("accept() error");
+
+    while ((strLen = recv(hClntSock, message, BUFSIZE, 0)) != 0) { /* 데이터 수신 및 전송 */
+        send(hClntSock, message, strLen, 0);
+    }
+
+    closesocket(hClntSock);
+    WSACleanup();
+
+    return 0;
 }
 
-void error_handling(char *message)
+void ErrorHandling(char* message)
 {
-	fputs(message, stderr);
-	fputc('\n', stderr);
-	exit(1);
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
 }
 
